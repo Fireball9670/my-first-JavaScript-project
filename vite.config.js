dde1f7e3a75a01bb4b82291f2fe22e94c742115f
Henri <fireball9670@outlook.de>
2025-06-02 17:29:28 +0200

    testing vite config

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].js', // Puts scripts at root
+        chunkFileNames: '[name].js', // Puts chunks at root
+        assetFileNames: (assetInfo) => {
+          const extType = path.extname(assetInfo.name);
+          if (extType === '.css') return '[name][extname]'; // Puts CSS at root
+          return 'assets/[name][extname]'; // Other assets (e.g., fonts, images)
+        },
+      },
+    },
+  },
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'assets/**/*',      // Copies everything from root-level assets
+          dest: 'assets'           // To dist/assets
+        }
+      ]
+    })
+  ]
+})
